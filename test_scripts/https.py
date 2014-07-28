2264c2e2b717e70af10c5f4a636805e2f16d5efa
tom hensel <tom@jitter.eu>
2014-07-28 15:20:13 +0200

    migrate to "requests" library from "urllib2" use class inheritance for configuration cleanup

--- test_scripts/https.py
+++ test_scripts/https.py
@@ -1,53 +1,58 @@
+# tom hensel <tom@jitter.eu> for CIPHRON
+
+import requests
 import hashlib
 import time
-import urllib2
+import random
 
-class Transaction(object):
+class NotConfiguredException(Exception):
+    pass
+
+class LoadTest(object):
     def __init__(self):
-        self.custom_timers = {}
+        # self.custom_timers = {}
+        raise NotConfiguredException('foo')
 
     def run(self):
-        # id of testcase
-        test_id = 'mib-jira.iavtech.net_ssl'
-
-        # request timeout
-        http_timeout = 10
-
-        # http server to profile
-        url = 'https://mib-jira.iavtech.net/'
-
-        # initialize urllib
-        req = urllib2.Request(url = url)
-
-        # set 'User-Agent' header
-        user_agent = 'Mozilla/4.0 (compatible; Testing)'
-        req.add_header('User-Agent', user_agent)
-
-        # set 'Authorization' header
-#        auth = 'Basic ZnJpZW5kbHl1c2VyOiFIQTJyVXBf'
-#        req.add_header('Authorization', auth)
-
         # start measurement
         start_timer = time.time()
 
-        # send request, get response
-        response = urllib2.urlopen(req, timeout = http_timeout)
+        # initialize session
+        s = requests.Session()
+        s.max_redirects = self.http_max_redirects
+        s.headers = self.http_headers
+        s.verify = self.https_verify_cert
 
-        # retrieve content (which adds a bit of delay, too)
-        content = response.read().strip()
+        # do request
+        r = s.get(self.http_url, timeout=self.http_timeout, params=self.params)
 
         # stop measurement
         latency = time.time() - start_timer
 
         # store latency
-        self.custom_timers[test_id] = latency
+        self.custom_timers[r.status_code] = latency
 
         # verbose info
-        print 'url:', response.geturl(), ', code:', response.code, ', hash:', hashlib.sha224(content).hexdigest()
+        print 'url:', r.url, ', code:', r.status_code, ', reason:', r.reason, ', elapsed:', r.elapsed, 'hash:', hashlib.sha224(r.text).hexdigest()
 
         # asset response code of 200 (HTTP OK)
-        #assert (response.code == 200), 'Bad Response: HTTP %s' % response.code
-        assert (response.code == 401), 'Bad Response: HTTP %s' % response.code
+        assert (r.status_code == requests.codes.ok), 'Bad Response: HTTP Code %s' % r.status_code
 
         # assert known string in content
         #assert ('iav' in content), 'Text Assertion Failed'
+
+class Transaction(LoadTest):
+    def __init__(self):
+        self.custom_timers = {}
+
+        # configuration
+        self.test_id = 'mib-jira.iavtech.net_ssl'
+        self.http_headers = {'User-Agent': 'Mozilla/4.0 (compatible; Testing)'}
+        self.http_timeout = 5
+        self.http_max_redirects = 10
+        self.https_verify_cert = False
+        self.http_url = 'https://mib-jira.iavtech.net/'
+        self.params = {'randomId': random.uniform(1, 10)}
+        #self.auth = 'Basic ZnJpZW5kbHl1c2VyOiFIQTJyVXBf'
+
+#
